cd8bef466e090c59482db278e13b8f016ba492d1
moeenuddin420 <moeenu321@gmail.com>
2025-12-11 13:51:28 +0600

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,21 @@
-/* SAVE AS: service-worker.js */
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
-
-const firebaseConfig = {
-    apiKey: "AIzaSyAafXkJwyZ5F7Xuax0VktZ9cpqWD4oCvxU",
-    authDomain: "tournament-97743.firebaseapp.com",
-    projectId: "tournament-97743",
-    messagingSenderId: "584797187828",
-    appId: "1:584797187828:web:4c643f83dfd9b700adb8a1"
-};
-
-firebase.initializeApp(firebaseConfig);
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.data.title;
-  const notificationOptions = {
-    body: payload.data.body,
-    icon: 'https://cdn-icons-png.flaticon.com/512/3233/3233483.png'
-  };
-  
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+{
+  "name": "G-ZONE ESPORTS",
+  "short_name": "G-ZONE",
+  "start_url": "index.html",
+  "display": "standalone",
+  "background_color": "#0f172a",
+  "theme_color": "#0f172a",
+  "orientation": "portrait",
+  "icons": [
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/3233/3233483.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "https://cdn-icons-png.flaticon.com/512/3233/3233483.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
